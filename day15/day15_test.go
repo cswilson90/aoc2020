a4aba3a4e228e733ed02f1f2c09409b9d59c3912
Chris Wilson <cswilson90@gmail.com>
2020-12-15 19:42:49 +0000

    Parallelise day 15 tests

--- day15/day15_test.go
+++ day15/day15_test.go
@@ -1,39 +1,32 @@
 package day15
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestExample(t *testing.T) {
-	tests := []struct {
-		input   []int
-		answer1 int
-		answer2 int
-	}{
-		{[]int{0, 3, 6}, 436, 175594},
-		// Commented out for speed as part 2 is quite slow
-		//{ []int{1, 3, 2}, 1, 2578 },
-		//{ []int{2, 1, 3}, 10, 3544142 },
-		//{ []int{1, 2, 3}, 27, 261214 },
-	}
-
-	for _, test := range tests {
-		answer := PlayMemoryGame(test.input, 2020)
-		assert.Equal(t, test.answer1, answer)
-
-		answer = PlayMemoryGame(test.input, 30000000)
-		assert.Equal(t, test.answer2, answer)
-	}
+var tests = []struct {
+	input   []int
+	answer1 int
+	answer2 int
+}{
+	{[]int{0, 3, 6}, 436, 175594},
+	{[]int{1, 3, 2}, 1, 2578},
+	{[]int{2, 1, 3}, 10, 3544142},
+	{[]int{1, 2, 3}, 27, 261214},
+	{[]int{19, 0, 5, 1, 10, 13}, 1015, 201},
 }
 
-func TestDay(t *testing.T) {
-	input := []int{19, 0, 5, 1, 10, 13}
+func TestExample(t *testing.T) {
+	for i, tc := range tests {
+		tc := tc
+		t.Run(fmt.Sprintf("Test %v", i), func(t *testing.T) {
+			t.Parallel()
 
-	answer := PlayMemoryGame(input, 2020)
-	assert.Equal(t, 1015, answer, "Part 1 Incorrect")
-
-	answer = PlayMemoryGame(input, 30000000)
-	assert.Equal(t, 201, answer, "Part 2 Incorrect")
+			assert.Equal(t, tc.answer1, PlayMemoryGame(tc.input, 2020))
+			assert.Equal(t, tc.answer2, PlayMemoryGame(tc.input, 30000000))
+		})
+	}
 }
